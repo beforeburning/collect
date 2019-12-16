e231e4c959b53f288377f70c98bb16a4b36d7329
鲁超凡 <luchaofan@jiankang.com>
2019-12-16 15:08:17 +0800

    ua

--- src/common/domObtain/index.ts
+++ src/common/domObtain/index.ts
@@ -12,7 +12,7 @@ import Interface from './Interface';
 
 export default class DomObtain implements Interface {
 
-    UA: string = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3642.0 Safari/537.36';
+    UA: string = ' ';
     link: string;
 
     constructor(link: string) {
